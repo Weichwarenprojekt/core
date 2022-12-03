4261eaba6904acef2e97b07ab86ead78aad9b699
Stefan Rink <rinkyy.sr@googlemail.com>
2022-12-03 15:30:07 +0100

    chore: add publishing/versioning workflow

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -9,7 +9,7 @@
 
 import { readCachedProjectGraph } from 'nx/src/project-graph/project-graph';
 import { execSync } from 'child_process';
-import { readFileSync, writeFileSync } from 'fs';
+import { readFileSync } from 'fs';
 import chalk from 'chalk';
 
 function invariant(condition, message) {
@@ -19,16 +19,9 @@ function invariant(condition, message) {
   }
 }
 
-// Executing publish script: node path/to/publish.mjs {name} --version {version} --tag {tag}
-// Default "tag" to "next" so we won't publish the "latest" tag by accident.
-const [, , name, version, tag = 'next'] = process.argv;
-
-// A simple SemVer validation to validate the version
-const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
-invariant(
-  version && validVersion.test(version),
-  `No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${version}.`
-);
+// Executing publish script: node path/to/publish.mjs {name} --tag {tag}
+// Default "tag" to "next" so we won't publish the "latest" tag by accident. rinks: nah, do it!
+const [, , name, tag = 'latest'] = process.argv;
 
 const graph = readCachedProjectGraph();
 const project = graph.nodes[name];
@@ -49,8 +42,13 @@ process.chdir(outputPath);
 // Updating the version in "package.json" before publishing
 try {
   const json = JSON.parse(readFileSync(`package.json`).toString());
-  json.version = version;
-  writeFileSync(`package.json`, JSON.stringify(json, null, 2));
+
+  // A simple SemVer validation to validate the version
+  const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
+  invariant(
+    json.version && validVersion.test(json.version),
+    `No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${json.version}.`
+  );
 } catch (e) {
   console.error(
     chalk.bold.red(`Error reading package.json file from library build output.`)
