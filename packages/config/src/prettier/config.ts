88eaccc20c67d35f53bacd9d3860ae5f3d7ef37d
Stefan Rink <rinkyy.sr@googlemail.com>
2023-01-04 16:50:31 +0100

    feat(configuration): init configuration
    moved from old config package

--- packages/config/src/prettier/config.ts
+++
@@ -1,9 +0,0 @@
-import { PrettierConfigModel } from "./PrettierConfigModel";
-
-export const prettierConfig = {
-    singleQuote: false,
-    trailingComma: "all",
-    tabWidth: 4,
-    printWidth: 120,
-    endOfLine: "auto",
-} as PrettierConfigModel;
