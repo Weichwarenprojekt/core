e36d9d140ec83323b62e74a9dc764d4bf5954161
Stefan Rink <rinkyy.sr@googlemail.com>
2023-04-27 21:59:47 +0200

    fix(configuration): use not deprecated jsdoc eslint rules

--- packages/configuration/src/eslint/base.config.ts
+++ packages/configuration/src/eslint/base.config.ts
@@ -5,24 +5,13 @@ export default {
   root: true,
   parser: '@typescript-eslint/parser',
   ignorePatterns: ['.eslintrc.*', '.prettierrc.*'],
-  plugins: ['prettier', '@typescript-eslint/eslint-plugin'],
+  plugins: ['prettier', '@typescript-eslint/eslint-plugin', 'eslint-plugin-jsdoc'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:jsdoc/recommended'
   ],
   rules: {
-    'require-jsdoc': [
-      'error',
-      {
-        require: {
-          FunctionDeclaration: true,
-          MethodDefinition: true,
-          ClassDeclaration: true,
-          ArrowFunctionExpression: true,
-          FunctionExpression: true,
-        },
-      },
-    ],
-    'no-console': 'error',
+    'no-console': 'warn',
   },
 };
